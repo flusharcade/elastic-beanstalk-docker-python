f2c71d79220c8f1774297e8e4b93dcc68f9e8a3a
Michael Williams <mwilliams@flusharcade.com>
2018-12-15 16:31:47 +1100

    Added missing header func in app.py.

--- app/app.py
+++ app/app.py
@@ -1,12 +1,20 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 
+
 class Handler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
     def do_GET(self):
+        self._set_headers()
         hello_world_string = 'Hello World!'
-        
+
         # return string to test api is working
-        self.wfile.write(actionResult.encode())
+        self.wfile.write(hello_world_string.encode())
+
 
 port = 5000
 print('Listening on localhost:%s' % port)
